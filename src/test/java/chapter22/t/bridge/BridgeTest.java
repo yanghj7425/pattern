53f269c0a449e612e37740ca5fc2955e68f05d16
yanghj <1501587673@qq.com>
2018-07-02 23:51:26 +0800

    drop blank line

--- src/test/java/chapter22/t/bridge/BridgeTest.java
+++ src/test/java/chapter22/t/bridge/BridgeTest.java
@@ -12,7 +12,6 @@ public class BridgeTest {
         HandsetBrandA handsetBrandA = new HandsetBrandA();
         handsetBrandA.attach(new HandsetTel());
         handsetBrandA.attach(new HandsetGame());
-
         handsetBrandA.run("game");
     }
 }
